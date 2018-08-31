3f8ee093a55123c953640d8367000487483b459e
Stalin Sánchez <ssanchez684@gmail.com>
2018-08-31 00:12:55 -0400

    Agregando token verificación de google

--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <!-- Meta, title, CSS, favicons, etc. -->
+    <meta name="google-site-verification" content="cFCM0HW0yF0MTiqS1JbbkGubvNtLNzszQt2ORucFVV0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
